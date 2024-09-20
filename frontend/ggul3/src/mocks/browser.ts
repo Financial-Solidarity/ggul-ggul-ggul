1270508574d874b9314b6f58a152f7880d96e4aa
youngentry <gentry_@naver.com>
2024-09-20 14:15:06 +0900

    Revert "Merge branch 'fe-chore/10-msw-setting' into 'master'"
    This reverts commit a2d4bcca805198c8f489e3df98fe745fa26536f4, reversing
    changes made to 0f4204b9f12ff8b967b69efebc84eda8b0256e4a.

--- frontend/ggul3/src/mocks/browser.ts
+++
@@ -1,6 +0,0 @@
-import { setupWorker } from 'msw/browser';
-
-import { exampleHandlers } from './domain1/domain1Handlers';
-
-// 직접 작성한 핸들러를 setupWorker에서 import 하여 사용합니다. ex)domain1Handlers.ts 파일 코드 참고
-export const worker = setupWorker(...exampleHandlers);
