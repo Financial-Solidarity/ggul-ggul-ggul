4dcea81c78928fd1ba7bd55a31f5792e4dc70116
youngentry <gentry_@naver.com>
2024-09-20 13:48:01 +0900

    fe/chore: MSW 예시 사용 코드 삭제

--- frontend/ggul3/src/App.tsx
+++ frontend/ggul3/src/App.tsx
@@ -1,25 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
 
 import Temp from './modules/domain1/pages/Temp';
-import { mockRequest } from './mocks/wrapper';
 
 function App() {
-  const tempRequest = async () => {
-    try {
-      const response = await fetch('/api/users');
-      const data = await response.json();
-
-      return data; // 데이터 반환
-    } catch (error) {
-      console.error('Error fetching mock data:', error);
-    }
-  };
-
-  useEffect(() => {
-    mockRequest(tempRequest);
-  }, []);
-
   return (
     <Routes>
       <Route element={<Temp />} path="/" />
