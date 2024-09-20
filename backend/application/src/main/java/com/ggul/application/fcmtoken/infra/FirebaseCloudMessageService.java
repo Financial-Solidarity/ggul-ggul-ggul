272fd9cc792636d9514635b36803721aed2dab54
김현진 <khj745700@naver.com>
2024-09-20 18:01:36 +0900

    be/feat: fcm 등록 및 전송 기능 추가 (ing)
    #14

---
+++ backend/application/src/main/java/com/ggul/application/fcmtoken/infra/FirebaseCloudMessageService.java
@@ -0,0 +1,39 @@
+package com.ggul.application.fcmtoken.infra;
+
+import com.google.firebase.messaging.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@Slf4j
+public class FirebaseCloudMessageService {
+
+
+    public String sendDataMessageTo(Integer notificationId, String targetToken, String title, String body, String type, String target) throws FirebaseMessagingException {
+
+        Message msg = Message.builder()
+                .putData("time", LocalDateTime.now().toString())
+                .putData("notification_id", notificationId != null ? notificationId.toString() : "null")
+                .putData("title", title)
+                .putData("body", body)
+                .putData("type", type)
+                .putData("target", target)
+                .setToken(targetToken)
+                .setApnsConfig(ApnsConfig.builder().setAps(Aps.builder().setContentAvailable(true).build()).putHeader("apns-priority", "10").build())
+//                .setNotification(Notification.builder()
+//                        .setBody(body).setTitle(title)
+//                        .build())
+                .setWebpushConfig(WebpushConfig.builder().setNotification(WebpushNotification.builder().setBody(body).setTitle(title).build()).build())
+                .setAndroidConfig(AndroidConfig.builder().setPriority(AndroidConfig.Priority.HIGH).build())
+                .build();
+
+        return sendMessageTo(msg);
+    }
+
+    public String sendMessageTo(Message message) throws FirebaseMessagingException {
+
+        return FirebaseMessaging.getInstance().send(message);
+    }
+}
