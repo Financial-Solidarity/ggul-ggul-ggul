bb1a67379b591b11910ac1a3332c3f70ef56be38
조성우 <he12569@gmail.com>
2024-09-23 04:08:46 +0900

    fe/feat: 의존성 추가 +  에셋 추가 1. NextUI & react-error-boundary 관련 의존성 추가 2. 게임 관련 이미지 에셋 추가

--- frontend/ggul3/tailwind.config.js
+++ frontend/ggul3/tailwind.config.js
@@ -13,7 +13,11 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+    },
   },
   darkMode: 'class',
   plugins: [
