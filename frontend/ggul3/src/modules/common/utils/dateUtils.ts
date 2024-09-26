1a35f5dd7ca9009bc60657966aaae9c671b491b6
박형준 <issg2030@gmail.com>
2024-09-26 10:47:56 +0900

    fe/feat: 오전 오후 포맷 출력함수
    #16

--- frontend/ggul3/src/modules/common/utils/dateUtils.ts
+++ frontend/ggul3/src/modules/common/utils/dateUtils.ts
@@ -1,6 +1,14 @@
 import dayjs from 'dayjs';
+
 export const toYYMDhm_ko = (date: string) => {
   return dayjs(date).format('YY년 M월 D일  H시 m분');
+};
+
+export const toAMPM_ko = (date: string) => {
+  const d = dayjs(date);
+  const ampm = d.hour() < 12 ? '오전' : '오후';
+
+  return `${ampm} ${d.format('h:mm')}`;
 };
 
 export const padZero = (num: number) => num.toString().padStart(2, '0');
