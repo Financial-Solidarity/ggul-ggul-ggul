4a201931d11ab3c5617f4349ec32f4397a8be039
김현진 <khj745700@naver.com>
2024-10-01 01:40:22 +0900

    be/feat: 채팅 전송 기능 추가
    #39

--- backend/application/src/main/java/com/ggul/application/chatting/application/ChattingRegisterService.java
+++ backend/application/src/main/java/com/ggul/application/chatting/application/ChattingRegisterService.java
@@ -1,10 +1,14 @@
 package com.ggul.application.chatting.application;
 
+import com.ggul.application.challange.domain.ChallengeParticipant;
+import com.ggul.application.chatting.application.dto.ChatRequest;
 import com.ggul.application.chatting.application.dto.JustificationChattingRequest;
 import com.ggul.application.chatting.domain.Chatting;
 import com.ggul.application.chatting.domain.repository.ChattingRepository;
+import com.ggul.application.chatting.domain.repository.ChattingRoomParticipantRepository;
 import com.ggul.application.chatting.exception.ChattingJustificationUnAuthorizedException;
 import com.ggul.application.chatting.exception.ChattingNotFoundException;
+import com.ggul.application.chatting.exception.ChattingRoomParticipantExistException;
 import com.ggul.application.chatting.query.ChattingSendService;
 import com.ggul.application.chatting.query.NotificationSender;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +21,7 @@ import java.util.UUID;
 @Service
 public class ChattingRegisterService {
     private final ChattingRepository chattingRepository;
+    private final ChattingRoomParticipantRepository chattingRoomParticipantRepository;
     private final ChattingSendService chattingSendService;
     private final NotificationSender notificationSender;
 
@@ -36,8 +41,21 @@ public class ChattingRegisterService {
                 .content(request.getContent())
                 .build();
 
-        chattingRepository.save(generate);
+        generate = chattingRepository.save(generate);
         chattingSendService.sendChat(generate);
         notificationSender.sendNotification(generate);
+    }
+
+    @Transactional
+    public void chattingCreate(ChatRequest request, UUID userId) {
+        ChallengeParticipant participant = chattingRoomParticipantRepository.findByChattingRoomIdAndUserId(request.getChattingRoomId(), userId).orElseThrow(ChattingRoomParticipantExistException::new);
+        Chatting chatting = Chatting.builder()
+                .participant(participant)
+                .type(Chatting.Type.COMMON)
+                .content(request.getContent())
+                .build();
+        chatting = chattingRepository.save(chatting);
+        chattingSendService.sendChat(chatting);
+        notificationSender.sendNotification(chatting);
     }
 }
--- backend/application/src/main/java/com/ggul/application/chatting/domain/repository/ChattingRoomParticipantRepository.java
+++ backend/application/src/main/java/com/ggul/application/chatting/domain/repository/ChattingRoomParticipantRepository.java
@@ -4,8 +4,10 @@ import com.ggul.application.challange.domain.ChallengeParticipant;
 import com.ggul.application.chatting.domain.ChattingRoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ChattingRoomParticipantRepository extends JpaRepository<ChattingRoomParticipant, UUID> {
@@ -15,4 +17,7 @@ public interface ChattingRoomParticipantRepository extends JpaRepository<Chattin
 
     @Query("SELECT cp FROM ChattingRoomParticipant crp  JOIN ChallengeParticipant cp ON crp.challengeParticipant = cp WHERE crp.chattingRoom.id = :chattingRoomId")
     List<ChallengeParticipant> findAllChallengeParticipantByChattingRoomId(UUID chattingRoomId);
+
+    @Query("SELECT cp FROM ChattingRoomParticipant crp JOIN ChallengeParticipant cp on crp.challengeParticipant = cp WHERE crp.chattingRoom.id = :chattingRoomId AND cp.user.id = :userId")
+    Optional<ChallengeParticipant> findByChattingRoomIdAndUserId(@Param("chattingRoomId") UUID chattingRoomId, @Param("userId") UUID userId);
 }
