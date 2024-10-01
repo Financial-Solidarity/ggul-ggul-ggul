60c0e3aa8216b3511b533daf8d5db5a92910aba2
박형준 <issg2030@gmail.com>
2024-10-02 01:12:20 +0900

    fe/feat: 최근 채팅 내역 조회 API
    #51

--- frontend/ggul3/src/modules/challenge/apis/chattingroom.ts
+++ frontend/ggul3/src/modules/challenge/apis/chattingroom.ts
@@ -1,9 +1,23 @@
-import { ChattingRoomGroupDTO } from '@types';
+import { Chat, ChattingRoomGroupDTO } from '@types';
 
 import { _axios } from '@/modules/common/utils/axios';
 
+/**
+ * 참여중인 채팅방 목록 조회
+ */
 export const getChattingRooomList = () => {
   return _axios<ChattingRoomGroupDTO[]>({
     url: 'challenges',
+    method: 'GET',
+  });
+};
+
+/**
+ * 최근 채팅 목록 조회 (읽지 않은 채팅 조회)
+ */
+export const getRecentChattingList = (chattingRoomId: string) => {
+  return _axios<Chat[]>({
+    url: `chattingRoom/${chattingRoomId}/chat/log/after`,
+    method: 'GET',
   });
 };
