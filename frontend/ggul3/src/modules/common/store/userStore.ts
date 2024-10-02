d7004bdcfa98cd371fbe5588a56c996b9f43f88a
youngentry <gentry_@naver.com>
2024-10-02 19:46:14 +0900

    fe/feat: 유저 정보 localStorage 에 저장

--- frontend/ggul3/src/modules/common/store/userStore.ts
+++ frontend/ggul3/src/modules/common/store/userStore.ts
@@ -31,10 +31,10 @@ export const useUserStore = create<UserState>()(
   persist(
     (set) => ({
       user: {
-        id: 0,
-        userId: '테스트 uuid',
-        username: '테스트',
-        nickname: '테스트',
+        id: -1,
+        userId: '',
+        username: '',
+        nickname: '',
         profileImg:
           'https://solsolhighasset.s3.ap-northeast-2.amazonaws.com/user%2Fprofile%2F01920d13-21f2-7f43-aa95-d8addad2c1bejpg',
       },
