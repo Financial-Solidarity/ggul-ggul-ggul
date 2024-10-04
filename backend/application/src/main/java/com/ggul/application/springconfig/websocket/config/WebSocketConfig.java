d19281a4497569188bd3c66d67d7e276dc6a84e0
김현진 <khj745700@naver.com>
2024-10-04 10:06:41 +0900

    be/fix: allowedOriginPattern 수정

--- backend/application/src/main/java/com/ggul/application/springconfig/websocket/config/WebSocketConfig.java
+++ backend/application/src/main/java/com/ggul/application/springconfig/websocket/config/WebSocketConfig.java
@@ -23,10 +23,10 @@ public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfig
     public void configureStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/connection") // ex ) ws://localhost:8080/stomp/chat
                 .addInterceptors(new HttpSessionHandshakeInterceptor())
-                .setAllowedOrigins("http://localhost:5173");
+                .setAllowedOriginPatterns("*");
         registry.addEndpoint("/stomp/connection") // ex ) ws://localhost:8080/stomp/chat
                 .addInterceptors(new HttpSessionHandshakeInterceptor())
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
