9416dd163b529526b79a9a1793f5fc93465f3b58
김현진 <khj745700@naver.com>
2024-10-08 01:53:35 +0900

    be/fix: spentAt pattern 수정

--- backend/application/src/main/java/com/ggul/application/payment/ui/dto/ConsumptionLogView.java
+++ backend/application/src/main/java/com/ggul/application/payment/ui/dto/ConsumptionLogView.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class ConsumptionLogView {
     private String productName;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH-mm-ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime spentAt;
     private Integer money;
     private String label;
