48764a277136dfd741889a4448de3542f8f0ce75
박형준 <issg2030@gmail.com>
2024-10-07 11:27:30 +0900

    fe/feat: 채팅방 없을 때 컨텐츠 추가
    #71

--- frontend/ggul3/src/modules/challenge/pages/ChattingRoomListPage.tsx
+++ frontend/ggul3/src/modules/challenge/pages/ChattingRoomListPage.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import { Button } from '@nextui-org/react';
+import { useNavigate } from 'react-router-dom';
 
 import { useGetChattingRoomList } from '../reactQueries/useChattingRoomQuery';
 import { ChattingRoomGroup } from '../components/chattingRoomList/ChattingRoomGroup';
@@ -10,12 +12,20 @@ import { useSetBottomBar } from '@/modules/common/hooks/useSetBottomBar';
 import { NavTitle } from '@/modules/common/components';
 import { BackButton } from '@/modules/common/components/BackButton/BackButton';
 import { NotificationButton } from '@/modules/common/components/NotificationButton/NotificationButton';
+import { NothingLottie } from '@/modules/game/components/common/Lotties/NothingLottie';
+import { PathNames } from '@/router';
 
 export const ChattingRoomListPage = () => {
   useSetBottomBar({ active: true, isDarkMode: false });
   const { chattingRoomList, setChattingRoomList } =
     useSocketChattingRoomListStore();
   const { data } = useGetChattingRoomList();
+
+  const navigate = useNavigate();
+
+  const toChallengeList = () => {
+    navigate(PathNames.CHALLENGE.MAIN.path);
+  };
 
   useEffect(() => {
     if (!data) return;
@@ -30,14 +40,24 @@ export const ChattingRoomListPage = () => {
         right={<NotificationButton color="black" />}
       />
       <PageContainer>
-        <div className="mb-20 flex flex-col gap-4 py-4">
-          {chattingRoomList.map((chattingRoomGroup) => (
-            <ChattingRoomGroup
-              key={chattingRoomGroup.challenge.challengeId}
-              {...chattingRoomGroup}
-            />
-          ))}
-        </div>
+        {chattingRoomList.length === 0 ? (
+          <div className="flex h-full flex-col items-center justify-center gap-2">
+            <NothingLottie />
+            <p className="text-center text-default-500">채팅방이 없어요</p>
+            <Button color="primary" onClick={toChallengeList}>
+              챌린지 하러가기
+            </Button>
+          </div>
+        ) : (
+          <div className="mb-20 flex flex-col gap-4 py-4">
+            {chattingRoomList.map((chattingRoomGroup) => (
+              <ChattingRoomGroup
+                key={chattingRoomGroup.challenge.challengeId}
+                {...chattingRoomGroup}
+              />
+            ))}
+          </div>
+        )}
       </PageContainer>
     </>
   );
