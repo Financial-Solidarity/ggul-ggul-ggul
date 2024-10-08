9e4e048f77af996456344f4620797f315eb7bcf0
김현진 <khj745700@naver.com>
2024-10-09 00:44:51 +0900

    be/feat: 챌린지 시작 준비 완료 웹소켓 전송 기능 추가
    #84

--- backend/application/src/main/java/com/ggul/application/challange/handler/ChallengeParticipantSendWebSocketHandler.java
+++ backend/application/src/main/java/com/ggul/application/challange/handler/ChallengeParticipantSendWebSocketHandler.java
@@ -1,19 +1,25 @@
 
 package com.ggul.application.challange.handler;
 
+import com.ggul.application.challange.domain.Challenge;
 import com.ggul.application.challange.domain.ChallengeParticipant;
+import com.ggul.application.challange.domain.ChallengeParticipantType;
+import com.ggul.application.challange.domain.CompetitionType;
 import com.ggul.application.challange.domain.repository.ChallengeParticipantRepository;
+import com.ggul.application.challange.domain.repository.ChallengeRepository;
 import com.ggul.application.challange.event.ChallengeParticipantChangedEvent;
+import com.ggul.application.challange.event.ChallengeReadiedEvent;
+import com.ggul.application.challange.exception.ChallengeNotFoundException;
 import com.ggul.application.common.infra.websocket.WebsocketSender;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.util.List;
 import java.util.Objects;
@@ -24,15 +30,16 @@ import java.util.UUID;
 public class ChallengeParticipantSendWebSocketHandler {
     private final WebsocketSender websocketSender;
     private final ChallengeParticipantRepository challengeParticipantRepository;
+    private final ChallengeRepository challengeRepository;
 
     @Async
-    @EventListener
+    @TransactionalEventListener
     @Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW)
     public void teamChangedEventHandler(ChallengeParticipantChangedEvent event) {
         List<ChallengeParticipant> participants = challengeParticipantRepository.findAllByChallenge_Id(event.getChallengeId());
         String messageType = getWebsocketType(event);
         participants.forEach(participant -> {
-            Response body = Response.builder()
+            ResponseForParticipantChange body = ResponseForParticipantChange.builder()
                     .challengeId(event.getChallengeId())
                     .team(event.getType().getType().name())
                     .isMine(Objects.equals(participant.getId(), event.getParticipantId()))
@@ -43,6 +50,34 @@ public class ChallengeParticipantSendWebSocketHandler {
                     .build();
             websocketSender.send(participant.getUser().getId(), messageType, body);
         });
+    }
+
+    @Async
+    @TransactionalEventListener
+    @Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW)
+    public void challengeReadiedEventHandler(ChallengeReadiedEvent event) {
+        Challenge challenge = challengeRepository.findById(event.getChallengeId()).orElseThrow(ChallengeNotFoundException::new);
+        List<ChallengeParticipant> allByChallengeId = challengeParticipantRepository.findAllByChallenge_Id(event.getChallengeId());
+
+        String messageType = "CHALLENGE_READY";
+        if (CompetitionType.TEAM.equals(challenge.getCompetitionType())) {
+            allByChallengeId.forEach(participant -> {
+                ResponseForChallengeReady body = ResponseForChallengeReady.builder()
+                        .challengeId(event.getChallengeId())
+                        .myTeamChattingRoomId(participant.getType().equals(ChallengeParticipantType.BLUE) ? event.getBlueTeamChattingRoomId() : event.getRedTeamChattingRoomId())
+                        .totalChattingRoomId(event.getTotalChattingRoomId())
+                        .build();
+                websocketSender.send(participant.getUser().getId(), messageType, body);
+            });
+        } else {
+            allByChallengeId.forEach(participant -> {
+                ResponseForChallengeReady body = ResponseForChallengeReady.builder()
+                        .challengeId(event.getChallengeId())
+                        .totalChattingRoomId(event.getTotalChattingRoomId())
+                        .build();
+                websocketSender.send(participant.getUser().getId(), messageType, body);
+            });
+        }
     }
 
 
@@ -61,7 +96,7 @@ public class ChallengeParticipantSendWebSocketHandler {
     @Getter
     @Builder
     @AllArgsConstructor
-    private static class Response {
+    private static class ResponseForParticipantChange {
         private UUID challengeId;
         private UUID participantId;
         private String nickname;
@@ -69,5 +104,14 @@ public class ChallengeParticipantSendWebSocketHandler {
         private Boolean isOwner;
         private Boolean isMine;
         private String team;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    private static class ResponseForChallengeReady {
+        private UUID challengeId;
+        private UUID myTeamChattingRoomId;
+        private UUID totalChattingRoomId;
     }
 }
