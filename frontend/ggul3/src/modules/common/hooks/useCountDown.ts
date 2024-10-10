e39ec96f9558ba85f41c483a777decd2b3c5e7a4
박형준 <issg2030@gmail.com>
2024-10-10 20:42:28 +0900

    fe/fix: useCountdown 시간 다되면 멈추도록

--- frontend/ggul3/src/modules/common/hooks/useCountDown.ts
+++ frontend/ggul3/src/modules/common/hooks/useCountDown.ts
@@ -32,6 +32,10 @@ export const useCountdown = (targetDate: string | Date): Countdown => {
   useEffect(() => {
     const timer = setInterval(() => {
       setTimeLeft(calculateTimeLeft());
+      // targetDate가 현재 시간을 지나면 타이머를 멈춤
+      if (new Date() >= new Date(targetDate)) {
+        clearInterval(timer);
+      }
     }, 1000); // 매 1초마다 업데이트
 
     return () => clearInterval(timer); // 컴포넌트 언마운트 시 타이머 정리
